fef9749209245b3487b79cfd117f05c427904820
FitroxIT <fitroxmk41@gmail.com>
2026-04-03 20:49:11 +0200

    Add files via upload

---
+++ MrBranch/script.js
@@ -0,0 +1,22 @@
+document.querySelectorAll('.add').forEach(button => {
+    button.addEventListener('click', () => {
+        const box = button.closest('.boxKamien');
+
+        const name = box.querySelector('.ruda').textContent;
+        const priceText = box.querySelector('[data-price]').getAttribute('data-price');
+        const price = parseFloat(priceText);
+        const img = box.querySelector('img').getAttribute('src');
+
+        const cart = JSON.parse(localStorage.getItem('cart')) || [];
+
+        const existing = cart.find(item => item.name === name);
+        if (existing) {
+            existing.quantity += 1;
+        } else {
+            cart.push({ name, price, quantity: 1, img });
+        }
+
+        localStorage.setItem('cart', JSON.stringify(cart));
+        //alert(`${name} dodano do koszyka!`);
+    });
+});
